0a26b952dbda4dce637cc225602b9bf89d8f223b
phomearadev <phomearadev@gmail.com>
2023-01-29 21:58:08 +0000

    screenshows

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default class App extends React.Component {
+
+  UNSAFE_componentWillMount() {
+    this.getOrientation();
+  }
+
+  render() {
+    return(
+    <View
+      onLayout={ () => this.handleLayoutChange}
+      style={styles.container}
+    >
+      <Text style={styles.text}>
+        {this.state.orientation}  
+      </Text>      
     </View>
-  );
+    );
+  }
+
+
+handleLayoutChange(){
+  this.getOrientation()
+}
+
+
+
+getOrientation() {
+  const { width, height } = Dimensions.get('window');
+  const orientation = height > width ? 'Portrait' : 'Landscape';
+  this.setState ({
+    orientation
+  });
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'   
   },
+  text: {
+    fontsize: 40,
+  }
 });
